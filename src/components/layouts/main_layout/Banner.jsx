32d322a8c2e60adce48e36d6d62bb802d1f2089a
SangBango <azka.khairul09@gmail.com>
2020-10-16 16:48:27 +0700

    change homepage

--- src/components/layouts/main_layout/Banner.jsx
+++ src/components/layouts/main_layout/Banner.jsx
@@ -34,12 +34,9 @@ export default class Banner extends Component {
                     <div className="col-lg-6 col-xl-6">
                         <div className="banner_text">
                         <div className="banner_text_iner">
-                            <h5 className="wow slideInRight">Every child yearns to learn</h5>
-                            <h1 className="wow fadeIn slow">Making Your Childs
-                            World Better</h1>
-                            <p>Replenish seasons may male hath fruit beast were seas saw you arrie said man beast whales
-                            his void unto last session for bite. Set have great you'll male grass yielding yielding
-                            man</p>
+                            <h5 className="wow slideInRight">easy pay, easy learn</h5>
+                            <h1 className="wow fadeIn slow">Different School lifestyle</h1>
+                            <p>OPSEKOLAH is a web-based application that can help your school's operations in the administration of payments using the "QRIS" method.</p>
                             <Link style={{color:"#fff"}} activeClass="active" to="products" spy={true} smooth={true} offset={-100} duration={500} className="btn_1 wow slideInUp">View Products </Link>
                             {this.renderRedirect()}
                             <div className="btn_2 wow slideInUp" onClick={this.goToRegistrationPage}>Get Started</div>
